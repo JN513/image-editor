93df036117ed2e60801d685c20ff0fada95bec01
Julio <jn5.guape@gmail.com>
2021-01-30 20:11:51 -0300

    tela inicial

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from kivy.app import App
 from kivy.lang.builder import Builder
+from kivy.core.window import Window
+
+Window.clearcolor = (.92, .92, .92, 1)
+
 from editor import editor
 
 bu = Builder.load_file("kvlang.kv")
