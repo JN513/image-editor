cf7050cb5157d44501b5914406d9c09d08d349ba
Julio <jn5.guape@gmail.com>
2021-01-30 18:59:19 -0300

    metodo cartoon image, exemplos de metodos e refatoramneto no save image

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 import cv2
 import numpy as np
-from PIL import Image, ImageEnhance
+from PIL import Image
 
 
 class Editor:
@@ -12,6 +12,7 @@ class Editor:
     image_extension = None
     image_history = list()
     history_position = None
+    original_image = None
 
     def reset_editor(self):
         self.image = None
@@ -21,9 +22,10 @@ class Editor:
         self.image_extension = None
         self.image_history = []
         self.history_position = None
+        self.original_image = None
 
     def pil_to_cv(self, image):
-        cv_image = np.array(self.image)
+        cv_image = np.array(image)
         cv_image = cv_image[:, :, ::-1].copy()
         return cv_image
 
@@ -33,18 +35,28 @@ class Editor:
 
     def load_image(self, image):
         try:
-            self.image = Image.open(image)
+            self.image = Image.open(image).convert("RGB")
             self.image_formate = self.image.format
-            self.image_name, self.extension = os.path.dirname(os.path.basename(image))
+            self.image_name, self.image_extension = os.path.splitext(
+                os.path.basename(image)
+            )
             self.image_path = os.path.dirname(os.path.realpath(image))
+            self.original_image = self.image
+            print("Imagem carregada com sucesso.")
             return True
         except:
+            print("Não foi possivel carregar a imagem.")
             return False
 
-    def save_image(self, path, image_name):
-        save_path = f"{path}/{image_name}.{self.image_extension}"
+    def save_image(self, path, image_name, extension=None):
+        if extension == None:
+            extension = self.image_extension
+        save_path = f"{path}/{image_name}{extension}"
         self.image.save(save_path, self.image_formate)
         self.reset_editor()
+
+    def reset_image(self):
+        self.image = self.original_image
 
     def smooth_image(self):
         cv_image = self.pil_to_cv(self.image)
@@ -119,12 +131,40 @@ class Editor:
         self.image = self.cv_to_pil(cv_image)
 
     def rotate_image(self, sentido="horario", angulo=90):
-        self.image_history.append(self.image.copy())
         if sentido == "horario":
             self.image = self.image.rotate(angulo * -1)
-        else:
+        elif sentido == "anti_horario":
             self.image = self.image.rotate(angulo)
 
-    def remove_color(self):
-        converter = ImageEnhance.Color(self.img)
-        self.img = converter.enhance(0)
+    def cartoon_image(self):
+        cv_image = self.pil_to_cv(self.image)
+        cv_image_gray = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
+        cv_image_gray = cv2.medianBlur(cv_image_gray, 5)
+        edges = cv2.adaptiveThreshold(
+            cv_image_gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 9, 9
+        )
+
+        color = cv2.bilateralFilter(cv_image, 9, 250, 250)
+        cartoon = cv2.bitwise_and(color, color, mask=edges)
+        self.image = self.cv_to_pil(cartoon)
+
+
+ed = Editor()
+ed.load_image("image.jpg")
+#ed.cartoon_image()
+#ed.rotate_image()
+#ed.rotate_image(sentido="anti_horario")
+#ed.rotate_image(sentido="anti_horario", angulo=180)
+#ed.rotate_image(angulo=170)
+#ed.gray_scale_image()
+#ed.adaptable_binary_image_gaussian()
+#ed.adaptable_binary_image_mean()
+#ed.binary_image_tozero()
+#ed.binary_image_tozero_reverse()
+#ed.binary_image_trunc()
+#ed.binary_image()
+#ed.binary_image_reverse()
+#ed.smooth_image()
+#ed.save_image("images", "gray_remo") #diretorio, e nome, opcional extenção
+#ed.save_image("images", "gray_remo", extension=".png") #diretorio, e nome, opcional extenção
+ed.image.show()
