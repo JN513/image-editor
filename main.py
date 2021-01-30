a1a2bb7495aefc1294b2e189465dfa4b2fd09735
Julio <jn5.guape@gmail.com>
2021-01-30 19:35:51 -0300

    titulo do programa

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from kivy.app import App
 from kivy.lang.builder import Builder
+from editor import editor
 
-bu = Builder.load_file('kvlang.kv')
+bu = Builder.load_file("kvlang.kv")
+
 
 class Program(App):
+    title = "Editor"
+
     def build(self):
         return bu
+
 
 if __name__ == "__main__":
     Program().run()
