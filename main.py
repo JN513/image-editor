7818560840401502301eba146aa6b11aaa5cd71a
Julio <jn5.guape@gmail.com>
2021-01-30 16:30:30 -0300

    init

---
+++ main.py
@@ -0,0 +1,34 @@
+import os
+import cv2
+import numpy
+from PIL import Image
+
+class Editor():
+    image = None
+    image_name = None
+    image_formate = None
+    image_path = None
+    image_extension = None
+    image_history = list()
+    history_position = None
+
+    def reset_editor(self):
+        pass
+
+    def open_image(self):
+        pass
+
+    def save_image(self):
+        pass
+
+    def binary_image(self):
+        pass
+
+    def adaptable_binary_image(self):
+        pass
+
+    def gray_scale_image(self):
+        pass
+
+    def rotate_image(self):
+        pass
